99e032bac00f9b72053d613469f9b06d10a93788
Roland Kajatin <33018844+Kajatin@users.noreply.github.com>
2018-11-24 13:27:29 +0100

    Update trajectory_generation.py

--- scripts/trajectory_generation.py
+++ scripts/trajectory_generation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import rospy as rp
 from geometry_msgs.msg import Transform
-from math import sin, cos, pi, atan, atan2
+from math import sin, cos, pi, sqrt, atan2
 
 beast=[0,0,0]
 target=[[1,0]]
+max_speed = 1
 
 def pos_ref_cb(data):
   global beast
@@ -23,11 +24,22 @@ rp.Subscriber('local_pos_ref', Transform, pos_ref_cb)
 
 while not rp.is_shutdown():
   if len(target) > 0:
-    P = target[0]
-    u = [P[0]-beast[0], P[1]-beast[1]]
-    phi_d = atan2(u[1], u[0])
-    e = phi_d - beast[2]
-    phi_e = atan2(sin(e),cos(e))
-    print(phi_e)
-    print(e)
+    P = target[0] # target
+    u = [P[0]-beast[0], P[1]-beast[1]] # vector from robot to target
+    u_mag = sqrt(u[0]**2 + u[1]**2) # distance to target
+    phi_d = atan2(u[1], u[0]) # desired heading
+    e = phi_d - beast[2] # difference between desired and current heading
+    phi_e = atan2(sin(e),cos(e)) # 4-quadrant angle of e
+    v = max_speed
+    if u_mag <= 0.5 and u_mag > 0.01:
+      v *= u_mag
+    else:
+      v *= 0.0
+      target.remove(P)
+    print('beast: ',beast)
+    print('P: ',P)
+    print('phi_e: ',phi_e)
+    print('v: ',v)
     print('******')
+  else:
+    print('no target')
