b77dec38001c366369f5bd95f083e15d1140bcac
Roland Kajatin <33018844+Kajatin@users.noreply.github.com>
2018-12-05 13:20:29 +0100

    changed node to service

--- scripts/waypoint_optimization.py
+++ scripts/waypoint_optimization.py
@@ -5,9 +5,10 @@ import sys
 import rospy as rp
 from geometry_msgs.msg import Transform
 from agv_as18.msg import Waypoints, Task
+from agv_as18.srv import *
 
 #locations
-BEAST = ['BEAST',70.0,220.0]
+BEAST = ['BEAST',0.0,0.0]
 AS = ['AS',170.0,125.0]
 C1 = ['C1',5.0,200.0]
 C2 = ['C2',5.0,170.0]
@@ -74,33 +75,33 @@ graph_left = {AS[0]:{MWP1[0]:AS_MWP1,MWP2[0]:AS_MWP2,MWP3[0]:AS_MWP3},MWP1[0]:{A
     C4[0]:np.sqrt((BEAST[1]-C4[1])**2+(BEAST[2]-C4[2])**2),C5[0]:np.sqrt((BEAST[1]-C5[1])**2+(BEAST[2]-C5[2])**2),C6[0]:np.sqrt((BEAST[1]-C6[1])**2+(BEAST[2]-C6[2])**2),MWP1[0]:np.sqrt((BEAST[1]-MWP1[1])**2+(BEAST[2]-MWP1[2])**2),MWP2[0]:np.sqrt((BEAST[1]-MWP2[1])**2+(BEAST[2]-MWP2[2])**2),
     MWP3[0]:np.sqrt((BEAST[1]-MWP3[1])**2+(BEAST[2]-MWP3[2])**2)}}
 
-# Product components with their locations
-P1 = [C1,C3,C4,C4]
-P2 = [C1,C2,C5,C6]
-P3 = [C3,C3,C5]
-P4 = [C2,C3,C4]    
+# # Product components with their locations
+# P1 = [C1,C3,C4,C4]
+# P2 = [C1,C2,C5,C6]
+# P3 = [C3,C3,C5]
+# P4 = [C2,C3,C4]    
 
-# Product components, only the names
-P_1 = [P1[0][0],P1[1][0],P1[2][0],P1[3][0]]
-P_2 = [P2[0][0],P2[1][0],P2[2][0],P2[3][0]]
-P_3 = [P3[0][0],P3[1][0],P3[2][0]]
-P_4 = [P4[0][0],P4[1][0],P4[2][0]]
+# # Product components, only the names
+# P_1 = [P1[0][0],P1[1][0],P1[2][0],P1[3][0]]
+# P_2 = [P2[0][0],P2[1][0],P2[2][0],P2[3][0]]
+# P_3 = [P3[0][0],P3[1][0],P3[2][0]]
+# P_4 = [P4[0][0],P4[1][0],P4[2][0]]
 
-# Products for which we need to bring components in order to start assembling
-'''
-ProductList=[]
-df = pd.read_csv('agv_assembly_order_train.csv')
-for element in df.values:
-    if element[0] == 1:
-        ProductList.append(P1)
-    elif element[0] == 2:
-        ProductList.append(P2)
-    elif element[0] == 3:
-        ProductList.append(P3)
-    elif element[0] == 4:
-        ProductList.append(P4)
-'''
-ProductList = [P1,P3,P2]
+# # Products for which we need to bring components in order to start assembling
+# '''
+# ProductList=[]
+# df = pd.read_csv('agv_assembly_order_train.csv')
+# for element in df.values:
+#     if element[0] == 1:
+#         ProductList.append(P1)
+#     elif element[0] == 2:
+#         ProductList.append(P2)
+#     elif element[0] == 3:
+#         ProductList.append(P3)
+#     elif element[0] == 4:
+#         ProductList.append(P4)
+# '''
+# ProductList = [P1,P3,P2]
 
 #dijkstra algorithm
 def dijkstra(graph,start,goal):
@@ -146,10 +147,42 @@ def pos_cb(data):
   BEAST[1]=data.translation.x
   BEAST[2]=data.translation.y
 
-def new_b_cb(data):
-    print('callback')
+# def new_b_cb(data):
+#     print('callback')
+#     b=[]
+#     task_sequence = data.task_seq
+#     for i in range(len(task_sequence)):
+
+#         if BEAST[1] > 52.5:
+#             graph = dict(graph_right)
+#         else:
+#             graph = dict(graph_left)
+
+#         c = dijkstra(graph,BEAST[0],task_sequence[i][0])
+
+#         for j in range(1,len(c)):
+#             b.append(c[j])
+#         BEAST[1] = task_sequence[i][1] 
+#         BEAST[2] = task_sequence[i][2]
+
+#     msg = Waypoints()
+#     for el in b:
+#         msg.b.append(el)
+#     for el in task_sequence:
+#         task = Task()
+#         task.name = el[0]
+#         task.x = el[1]
+#         task.y = el[2]
+#         msg.task_seq.append(task)
+#     waypoint_pub.publish(msg)
+
+
+def server_cb(req):
+    task_sequence = req.task_seq
+    # Initialize a list that will contain all the waypoints
     b=[]
-    task_sequence = data.task_seq
+
+    # apply dijkstras algorithm for every task in the task sequence list
     for i in range(len(task_sequence)):
 
         if BEAST[1] > 52.5:
@@ -157,31 +190,28 @@ def new_b_cb(data):
         else:
             graph = dict(graph_left)
 
+        # stores a list of the path the robot needs to take in order to complete a task
         c = dijkstra(graph,BEAST[0],task_sequence[i][0])
 
+        # Appends the path waypoints in a list
         for j in range(1,len(c)):
             b.append(c[j])
-        BEAST[1] = task_sequence[i][1] 
-        BEAST[2] = task_sequence[i][2]
 
-    msg = Waypoints()
+    print(b)
+
+
+    resp = PathResponse()
     for el in b:
-        msg.b.append(el)
-    for el in task_sequence:
-        task = Task()
-        task.name = el[0]
-        task.x = el[1]
-        task.y = el[2]
-        msg.task_seq.append(task)
-    waypoint_pub.publish(msg)
-
+        resp.b.append(el)
+    return resp
 
 
 rp.init_node('waypoint_optimization')
 rp.Subscriber('local_pos_ref', Transform, pos_cb)
-rp.Subscriber('update_b', Waypoints, new_b_cb)
-waypoint_pub = rp.Publisher('waypoints', Waypoints, queue_size=1)
+s = rp.Service('components', Path, server_cb)
 rp.spin()
+#rp.Subscriber('update_b', Waypoints, new_b_cb)
+#waypoint_pub = rp.Publisher('waypoints', Waypoints, queue_size=1)
 
 
 
@@ -191,49 +221,12 @@ rp.spin()
 
 
 
-# Create a list of lists containing the name and location of each destination the robot needs to go in order to take all the components needed and deliver them to the assembly
-task_sequence = [(ProductList[i])[j] for i in range(len(ProductList)) for j in range(len(ProductList[i]))]
-print(task_sequence)
-i = 2
-while i < len(task_sequence):
-    task_sequence.insert(i, AS)
-    i += 3
-task_sequence.append(AS)
 
-
-
-# Initialize a list that will contain all the waypoints
-b=[]
-
-# apply dijkstras algorithm for every task in the task sequence list
-for i in range(len(task_sequence)):
-
-    if BEAST[1] > 52.5:
-        graph = dict(graph_right)
-    else:
-        graph = dict(graph_left)
-
-    # stores a list of the path the robot needs to take in order to complete a task
-    c = dijkstra(graph,BEAST[0],task_sequence[i][0])
-
-    # Appends the path waypoints in a list
-    for j in range(1,len(c)):
-        b.append(c[j])
-
-    #Update the position of the robot    
-    BEAST[1] = task_sequence[i][1]
-    BEAST[2] = task_sequence[i][2] 
-
-print(b)
-
-
-msg = Waypoints()
-for el in b:
-    msg.b.append(el)
-for el in task_sequence:
-    task = Task()
-    task.name = el[0]
-    task.x = el[1]
-    task.y = el[2]
-    msg.task_seq.append(task)
-waypoint_pub.publish(msg)
+# # Create a list of lists containing the name and location of each destination the robot needs to go in order to take all the components needed and deliver them to the assembly
+# task_sequence = [(ProductList[i])[j] for i in range(len(ProductList)) for j in range(len(ProductList[i]))]
+# print(task_sequence)
+# i = 2
+# while i < len(task_sequence):
+#     task_sequence.insert(i, AS)
+#     i += 3
+# task_sequence.append(AS)
