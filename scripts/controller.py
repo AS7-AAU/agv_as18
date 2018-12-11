aecb5234a9b1b896bdf5a88a0b38f47fe19baf97
Roland Kajatin <33018844+Kajatin@users.noreply.github.com>
2018-12-11 19:59:22 +0100

    controller debug

--- scripts/controller.py
+++ scripts/controller.py
@@ -5,7 +5,7 @@ import rospy as rp
 from std_msgs.msg import Float32
 from agv_as18.msg import Motor, Reference
 
-P=5.0
+P=1.0
 I=0.0
 D=0.0
 freq=180.0
@@ -25,10 +25,10 @@ def encoder_right(enc_right):
 def cmd_vel_cb(data):
     global controller_left
     global controller_right
-    global omega_a
-    global omega_b
-    omega_a=data.v
-    omega_b=data.omega
+    # global omega_a
+    # global omega_b
+    # omega_a=data.v
+    # omega_b=data.omega
     controller_left.SetPoint = data.omega
     controller_right.SetPoint = data.v
 
@@ -42,7 +42,7 @@ def saturate(signal):
 controller_left = PID.PID(P,I,D)
 controller_right = PID.PID(P,I,D)
 
-controller_left.SetPoint = 0.0
+controller_left.SetPoint = 20.0
 controller_left.setSampleTime(1.0/freq)
 controller_right.SetPoint = 0.0
 controller_right.setSampleTime(1.0/freq)
@@ -54,8 +54,8 @@ rp.Subscriber("cmd_vel", Reference, cmd_vel_cb)
 pub = rp.Publisher("motor_signal", Motor, queue_size=1)
 rate = rp.Rate(freq)
 
-
 while not rp.is_shutdown():
-    #pub.publish(saturate(controller_right.output), saturate(controller_left.output))
+    omega_a += controller_right.output
+    omega_b += controller_left.output
     pub.publish(saturate(omega_a), saturate(omega_b))
     rate.sleep()
